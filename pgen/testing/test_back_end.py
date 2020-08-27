5a2f94227b3e33d8efa5d85429175c143c42a126
Cael Tintreach <CliffordMcKinney@gmail.com>
2020-08-27 11:11:58 +0100

    Implementing first version of testing for service 4.

---
+++ pgen/testing/test_back_end.py
@@ -0,0 +1,24 @@
+import unittest
+from flask import url_for
+from flask_testing import TestCase
+from application import app
+
+class TestBase(TestCase):
+	def create_app(self):
+		return app
+
+class TestView(TestBase):
+	def test_randomname(self):
+		response = self.client.get(url_for('prize'))
+		self.assertEqual(response.status_code, 200)
+
+class TestGen(TestBase):
+	def test_home_view(self):
+		response = self.client.post(
+			url_for('prize'),
+			data=dict(
+				number = "A",
+				letter = "A"
+			),
+		)
+		self.assertIn(b"no prize!", response.data)
