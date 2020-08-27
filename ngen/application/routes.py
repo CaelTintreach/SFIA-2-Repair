18486a8dec45dc05af38a4996073a960a7031e7c
Cael Tintreach <CliffordMcKinney@gmail.com>
2020-08-27 10:24:30 +0100

    Now repairing service 3 tests.

---
+++ ngen/application/routes.py
@@ -0,0 +1,18 @@
+from application import app
+from flask import request, Response 
+import random
+
+'''
+@app.route('/prize/ngen', methods = ['GET'])
+def nGen():
+	number = random.randint(1,10)
+	print(number)
+	return Response(number, mimetype='text/plain')
+
+print(nGen())
+'''
+
+@app.route('/prize/ngen', methods = ['GET'])
+def nGen():
+	prizePool = ["A", "B"]
+	return Response(random.choice(prizePool), mimetype='text/plain')
--- ngen/testing/test_back_end.py
+++ ngen/testing/test_back_end.py
@@ -9,12 +9,12 @@ class TestBase(TestCase):
 
 class TestView(TestBase):
 	def test_randomname(self):
-		response = self.client.get(url_for('lGen'))
+		response = self.client.get(url_for('nGen'))
 		self.assertEqual(response.status_code, 200)
 
 class TestGen(TestBase):
 	def test_gen(self):
-		response = self.client.get(url_for('lGen'))
+		response = self.client.get(url_for('nGen'))
 		check = False
 		for item in ['A','B']:
 			if bytes.decode(response.data) == item:
