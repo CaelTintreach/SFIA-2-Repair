3542a79b70324c833a3d0ecbbd7b6a5b4fa56a5b
Cael Tintreach <CliffordMcKinney@gmail.com>
2020-08-27 15:27:06 +0100

    DB integration testing.

--- ui/application/__init__.py
+++ ui/application/__init__.py
@@ -1,3 +1,11 @@
 from flask import Flask
 app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
+app.config['SECRET_KEY'] = str(os.getenv('MY_SECRET_KEY'))
+
+db = SQLAlchemy(app)
+
 from application import routes
