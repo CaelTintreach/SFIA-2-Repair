288e5832a5405c9550ece616422d21dfc1582efb
Cael Tintreach <CliffordMcKinney@gmail.com>
2020-08-27 15:51:33 +0100

    DB integration testing, fixed wrong class type.

--- ui/application/__init__.py
+++ ui/application/__init__.py
@@ -4,7 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
-app.config['SECRET_KEY'] = str(os.getenv('MY_SECRET_KEY'))
+#app.config['SECRET_KEY'] = str(os.getenv('MY_SECRET_KEY'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
