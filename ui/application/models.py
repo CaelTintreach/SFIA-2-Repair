4006be59aa9b901caf8dcbad9e893abc64e4eabc
Cael Tintreach <CliffordMcKinney@gmail.com>
2020-08-27 15:31:10 +0100

    DB integration testing, fixed wrong class type.

--- ui/application/models.py
+++ ui/application/models.py
@@ -1,6 +1,6 @@
 from application import db
 
-class prizesgiven(db.method):
+class prizesgiven(db.Model):
 	id = db.Column(db.Integer, primary_key=True)
 	pool = db.Column(db.String(20))
 	value = db.Column(db.String(20))
