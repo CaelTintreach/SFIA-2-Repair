1e062f1fa07375ab77b1194a37f8372aa0972837
Cael Tintreach <CliffordMcKinney@gmail.com>
2020-08-29 23:56:05 +0100

    Attempting Jenkins build.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 sudo groupadd -f docker
 sudo usermod -aG docker $(whoami)
+sudo chmod 666 /var/run/docker.sock
 #env DATABASE_URI="${DATABASE_URI}"
 #env MYSQL_ROOT_PASSWORD="${MYSQL_ROOT_PASSWORD}"
 docker-compose build
