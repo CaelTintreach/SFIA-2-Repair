23844e622f04a0f7eae1a949febc702f21ae43b3
Cael Tintreach <CliffordMcKinney@gmail.com>
2020-08-30 20:28:56 +0100

    Attempting Ansible.

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -4,5 +4,7 @@ sudo usermod -aG docker $(whoami)
 sudo chmod 666 /var/run/docker.sock
 env DATABASE_URI="${DATABASE_URI}"
 env MYSQL_ROOT_PASSWORD="${MYSQL_ROOT_PASSWORD}"
+echo $DATABASE_URI
+echo $MYSQL_ROOT_PASSWORD
 docker-compose build
 docker-compose push
