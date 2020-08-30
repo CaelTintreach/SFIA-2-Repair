55c409623396a5723e2a58404e3ae78e8bfbc315
Cael Tintreach <CliffordMcKinney@gmail.com>
2020-08-30 22:28:18 +0100

    Attempting Ansible.

--- ui/application/__init__.py
+++ ui/application/__init__.py
@@ -1,11 +1,5 @@
 from flask import Flask
 app = Flask(__name__)
-from flask_sqlalchemy import SQLAlchemy
 import os
-
-#app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI')).replace('"','')
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-#db = SQLAlchemy(app)
 
 from application import routes
