e09c8ad25734929ed268288d43df793f7e8b29c3
Cael Tintreach <CliffordMcKinney@gmail.com>
2020-08-31 14:19:08 +0100

    Attempting to add testing via scripting.

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,20 +1,20 @@
 #! /bin/bash
 cd lgen
 pip3 install -r requirements.txt
-python3 -m pytest --cov=. term-missing
+python3 -m pytest --cov=application --cov-report term-missing
 cd ..
 
 cd ngen
 pip3 install -r requirements.txt
-python3 -m pytest --cov=. term-missing
+pytest --cov application --cov-report term-missing
 cd ..
 
 cd ui1
 pip3 install -r requirements.txt
-python3 -m pytest --cov=. term-missing
+python3 -m pytest application --cov=. term-missing
 cd ..
 
 cd pgen
 pip3 install -r requirements.txt
-python3 -m pytest --cov=. term-missing
+python3 -m pytest pytest --cov=. term-missing
 cd ..
